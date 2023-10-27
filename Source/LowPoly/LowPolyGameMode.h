37290c420ebb7a009ac1be9bdadcb534eed455a6
Karnakhall <emil_q21@wp.pl>
2023-10-27 14:42:19 +0200

    Initial commit

---
+++ Source/LowPoly/LowPolyGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LowPolyGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALowPolyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALowPolyGameMode();
+};
+
+
+
