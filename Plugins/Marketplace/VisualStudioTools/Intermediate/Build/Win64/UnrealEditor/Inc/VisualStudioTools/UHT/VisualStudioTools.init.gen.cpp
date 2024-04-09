4d2522f5347cd00aa27c9696fec42745d84c02d9
Karnakhall <emil_q21@wp.pl>
2024-04-09 19:06:15 +0200

    update camera and movement character

--- Plugins/Marketplace/VisualStudioTools/Intermediate/Build/Win64/UnrealEditor/Inc/VisualStudioTools/UHT/VisualStudioTools.init.gen.cpp
+++ Plugins/Marketplace/VisualStudioTools/Intermediate/Build/Win64/UnrealEditor/Inc/VisualStudioTools/UHT/VisualStudioTools.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeVisualStudioTools_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000040,
-				0x055A979D,
+				0x3CCB4638,
 				0x8E6E4D9E,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCodeVisualStudioTools_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_VisualStudioTools.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_VisualStudioTools(Z_Construct_UPackage__Script_VisualStudioTools, TEXT("/Script/VisualStudioTools"), Z_Registration_Info_UPackage__Script_VisualStudioTools, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x055A979D, 0x8E6E4D9E));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_VisualStudioTools(Z_Construct_UPackage__Script_VisualStudioTools, TEXT("/Script/VisualStudioTools"), Z_Registration_Info_UPackage__Script_VisualStudioTools, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x3CCB4638, 0x8E6E4D9E));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
