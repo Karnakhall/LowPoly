22b285b508d87e2b89ae004366b061ab22ef7d22
KLMPLS <StoigniewDagobert@gmail.com>
2024-04-18 15:02:12 +0200

    Fixed double rotating of all platforms
    -Also slowed downed the big platforms if you were walking on them as they were rotating they would launch you at mach 3

--- Plugins/Marketplace/VisualStudioTools/Intermediate/Build/Win64/x64/UnrealEditor/Development/VisualStudioTools/Definitions.VisualStudioTools.h
+++ Plugins/Marketplace/VisualStudioTools/Intermediate/Build/Win64/x64/UnrealEditor/Development/VisualStudioTools/Definitions.VisualStudioTools.h
@@ -1,4 +1,4 @@
-#include "D:/VFX/LowPoly2/LowPoly/Intermediate/Build/Win64/x64/LowPolyEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
+#include "C:/Users/GniewkoPC/Documents/GitHub/LowPoly/Intermediate/Build/Win64/x64/LowPolyEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
 #undef UE_IS_ENGINE_MODULE
 #undef DEPRECATED_FORGAME
 #define DEPRECATED_FORGAME DEPRECATED
